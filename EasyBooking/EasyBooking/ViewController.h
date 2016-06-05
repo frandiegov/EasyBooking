cde5a411b928e86ac7ce5b49715a9bfcc6d0820b
Frandiego Veloz <fveloz@lcwmc-ml-01.local>
2016-06-05 14:29:08 -0400

    Started xcode project for App

---
+++ EasyBooking/EasyBooking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyBooking
+//
+//  Created by Frandiego Veloz on 6/5/16.
+//  Copyright © 2016 Gifted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EasyBooking/EasyBooking/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EasyBooking
+//
+//  Created by Frandiego Veloz on 6/5/16.
+//  Copyright © 2016 Gifted. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
