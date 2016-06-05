ca320fb0bfdb7affe19c603ca840a1b422bba0e3
Frandiego Veloz <fveloz@lcwmc-ml-01.local>
2016-06-05 16:46:24 -0400

    Added background and img buttons to app

--- EasyBooking/EasyBooking/ViewController.m
+++ EasyBooking/EasyBooking/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background2.jpg"]];
 }
 
 - (void)didReceiveMemoryWarning {
